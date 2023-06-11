7f27bdeaf56fd63804893c1b2b1557f91e94ba7a
vishnu4dev <vishnu@Vishnus-MacBook-Pro.local>
2023-06-12 01:25:49 +0530

    feat: email-send

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// 'S8D-oKGadXnGopa53' -pub
+// 'template_gr3zv3z' -tem
+// 'service_pajcker',- gmail
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,16 +37,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_pajcker',
+        'template_gr3zv3z',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Vishnu",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "vishnu@dev.vishnu",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'S8D-oKGadXnGopa53'
       )
       .then(
         () => {
@@ -118,7 +122,7 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-            {loading ? "Sending..." : "Skadooosh"}
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
       </motion.div>
